5c5d12374806a85ff4c91ad1bbccfc9696caee09
Masum Rana <masumrana446267@gmail.com>
2022-12-13 19:52:42 +0600

    fundameltal assignment added

--- js/applyJs.js
+++ js/applyJs.js
@@ -1,35 +1,17 @@
+                                      
+ const bestFriend={name:'Abul',Friend:'Babul'};
+ const bestFriend2={name:'Babul',Friend:'Abul'};
+
+const busFare=250;
+
+const LocalBusPublicLimit=50;
+
+const totalPublic=1200;
   
- console.log(inchis.value)
-  
-document.getElementById('btn').addEventListener('click',function(){
-    // get inchis from UI 
-    const inchis=document.getElementById('inchis');
-    const getInchis=inchis.value;
-    // Convert String To number 
-     const getInchisValue=parseInt(getInchis);
-    //  Get Feet Calculation
-     const getFeet=getInchisValue/12
-   const feetValue=  getFeet.toFixed(2)
- 
-    //  get Feet Input Field 
-const feets=document.getElementById('feets');
-feets.innerText=feetValue;
- 
- 
- 
-})
+function neetBus(publicQuantity){
+    return totalBusNeed;
+
+}
 
 
-// 1.Feet To meter 
-// Miles to Kilomerer 
-
-// F to C 
-
-// kj to gram 
-// gram to kj
-
-
-
-
-
-
+neetBus(totalPublic)
